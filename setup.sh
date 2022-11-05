94c9c94fe93c4cb8a2d7aa8fda3c919d297c552c
Adnan Shaikh <adnan.shaikh1806@gmail.com>
2022-11-05 12:26:27 -0700

    Updates to setup

--- setup.sh
+++ setup.sh
@@ -7,11 +7,25 @@ brew install fzf
 # To install useful key bindings and fuzzy completion:
 $(brew --prefix)/opt/fzf/install
 
+# Windows Style Alt-Tab Switcher
+brew install --cask alt-tab
+
+# Fast and user-friendly alternative to find
 brew install fd
+
+# Better version of unix's 'cat'
 brew install bat
+
+# Also known as 'ag'. Code-search similar to ack
 brew install the_silver_searcher
+
+# Download YouTube videos
 brew install yt-dlp
+
+# Manage EXIF metadata
 brew install exiftool
+
+# Split MP3s using cue files
 brew install mp3splt
 
 # Install python dependencies
